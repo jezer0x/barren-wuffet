55ae1288c7461bab73b4aeb045c791d341da5f57
Mad Neutrino <madneutrino@jezer0x.com>
2022-07-28 18:24:53 +0800

    Outline of RuleExecutor tests

--- test/RuleExecutor.ts
+++ test/RuleExecutor.ts
@@ -42,28 +42,7 @@ describe("RuleExecutor", () => {
     });
   });
 
-  describe("Add Triggers", () => {
-    it("Should revert with the right error if called from another account", async () => {
-      const { ruleExecutor, owner, otherAccount } = await loadFixture(
-        deployRuleExecutorFixture
-      );
-
-      // We use lock.connect() to send a transaction from another account
-      await expect(ruleExecutor.connect(otherAccount).addTriggerFeed("eth", "0xc0ffee254729296a45a3885639AC7E10F9d54979", "0x616d4bcd", [])).to.be.revertedWith(
-        "Ownable: caller is not the owner"
-      );
-    });
-
-    it("Should add a trigger feed if called by the owner", async () => {
-      const { ruleExecutor, owner } = await loadFixture(
-        deployRuleExecutorFixture
-      );
-      
-      await ruleExecutor.addTriggerFeed("eth", "0xc0ffee254729296a45a3885639AC7E10F9d54979", "0x616d4bcd", []);
-    });
-  });
-
-  describe("Add Rule", function() {
+  describe.skip("Add Rule By Anyone", () => {
     const validTrigger: REType.TriggerStruct = {
       op: 1,
       // TODO trigger requires 2 assets. this doesnt fit here.
@@ -71,58 +50,51 @@ describe("RuleExecutor", () => {
       value: 1000
     }
 
-    it("Should not allow adding a rule, where the trigger is not a whitelisted item", async () =>{
-      const { ruleExecutor, otherAccount } = await loadFixture(
-        deployRuleExecutorFixture
-      );            
-      
-      const trigger: REType.TriggerStruct = {
-        op: 0,
-        param: ethers.utils.defaultAbiCoder.encode([ "string", "string" ], [ "0x0", "0x0" ]),
-        value: 1000
-      }
-
-      const action: REType.ActionStruct = {
-        action: "backflip",
-        data: "0x000000000000000000000000000000000000000000000000000000000000acde",
-        fromToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979",
-        minTokenAmount: 1,
-        totalCollateralAmount: 1,
-        toToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979"
-      };
-
-      await expect(ruleExecutor.connect(otherAccount).addRule(trigger, action)).to.be.revertedWith(
-        "unauthorized trigger"
-      );      
+    const action: REType.ActionStruct = {
+      action: "backflip",
+      data: "0x000000000000000000000000000000000000000000000000000000000000acde",
+      fromToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979",
+      minTokenAmount: 1,
+      totalCollateralAmount: 1,
+      toToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979"
+    };
+    
+    it("Should revert if trigger doesnt have a callee with validateTrigger", async () => {
 
     });
-    it("Should not allow adding a rule, with a valid trigger where the action is not a whitelisted item", async () =>{
+
+    it("Should revert if validateTrigger on trigger does not return true", async () => {
+
+    });
+
+    it("Should revert if validateTrigger on trigger is not a view fn", async () => {
+
+    });
+
+    
+    it("Should revert if action doesnt have a callee with validateAction", async () => {
+
+    });
+
+    it("Should revert if validateAction on action does not return true", async () => {
+
+    });
+
+    it("Should revert if validateAction on action is not a view fn", async () => {
+
+    });
+
+    it("Should revert if trigger has not been whitelisted", async () => {
+
+    });
+
+    it("Should revert if action has not been whitelisted", async () => {
+
+    });
+
+
+    it("Should emit RuleCreated event if Trigger and Action are valid", async () => {
       const { ruleExecutor, owner, otherAccount } = await loadFixture(
-        deployRuleExecutorFixture
-      );
-      
-      // Add trigger feeds with valid wallets.
-      await ruleExecutor.addTriggerFeed("eth", ethers.Wallet.createRandom().address, "0x616d4bcd", []);
-      await ruleExecutor.addTriggerFeed("uni", ethers.Wallet.createRandom().address, "0x616d4bcd", []);      
-
-      const action: REType.ActionStruct = {
-        action: "backflip",
-        data: "0x000000000000000000000000000000000000000000000000000000000000acde",
-        fromToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979",
-        minTokenAmount: 1,
-        // this field indicates the total collateral attributed to this action.
-        // we shouldnt be passing this here.
-        totalCollateralAmount: 0, 
-        toToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979"
-      };
-
-      await expect(ruleExecutor.connect(otherAccount).addRule(validTrigger, action)).to.be.revertedWith(
-        "Action not supported"
-      );
-    });
-
-    it("Should allow adding a rule, for all whitelisted triggers and actions", async () =>{
-      const { ruleExecutor, otherAccount } = await loadFixture(
         deployRuleExecutorFixture
       );
 
@@ -151,8 +123,113 @@ describe("RuleExecutor", () => {
         totalCollateralAmount: 0, 
         toToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979"
       });
+      // use otherAccount to create the rule
+    });
+    it("Should not allow adding a rule, where the trigger is not a whitelisted item", async () =>{
+      const { ruleExecutor, otherAccount } = await loadFixture(
+        deployRuleExecutorFixture
+      );            
+      
+      const trigger: REType.TriggerStruct = {
+        op: 0,
+        param: ethers.utils.defaultAbiCoder.encode([ "string", "string" ], [ "0x0", "0x0" ]),
+        value: 1000
+      }
+
+    
+      await expect(ruleExecutor.connect(otherAccount).addRule(trigger, action)).to.be.revertedWith(
+        "unauthorized trigger"
+      );      
+
+    });
+    it("Should not allow adding a rule, with a valid trigger where the action is not a whitelisted item", async () =>{
+      const { ruleExecutor, owner, otherAccount } = await loadFixture(
+        deployRuleExecutorFixture
+      );
+      
+      // Add trigger feeds with valid wallets.
+      await ruleExecutor.addTriggerFeed("eth", ethers.Wallet.createRandom().address, "0x616d4bcd", []);
+      await ruleExecutor.addTriggerFeed("uni", ethers.Wallet.createRandom().address, "0x616d4bcd", []);      
+
+      const action: REType.ActionStruct = {
+        action: "backflip",
+        data: "0x000000000000000000000000000000000000000000000000000000000000acde",
+        fromToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979",
+        minTokenAmount: 1,
+        // this field indicates the total collateral attributed to this action.
+        // we shouldnt be passing this here.
+        totalCollateralAmount: 0, 
+        toToken: "0xc0ffee254729296a45a3885639AC7E10F9d54979"
+      };
+
+      await expect(ruleExecutor.connect(otherAccount).addRule(validTrigger, action)).to.be.revertedWith(
+        "Action not supported"
+      );
+    });  
+  });
+
+  describe.skip("Subscribe to Rule", function() {  
+    it("should not allow subscribing to a rule, if insufficient collateral is provided", async  () => {
+
+    });
+    
+    it("should allow subscribing to rule created by anyone, if exact or excess collateral is provided", async  () => {
+      // Subscribed event is created
+      // collateral is transferred
 
     });
 
   });
+
+  describe.skip("Check Rule", () => {
+    it("should return true if the checkTrigger on the callee denoted by ruleHash returns true", async () => {
+
+    });
+
+    it("should return false if the checkTrigger on the callee denoted by ruleHash returns false", async () => {
+
+    });
+
+    it("should return false if the checkTrigger is not available on the callee", async () => {
+
+    });
+
+  });
+
+  describe.skip("Execute Rule", () => {
+    it("Should revert if anyone tries to execute the rule, and the trigger is invalid", async () => {
+
+    });
+
+    it("Should allow anyone to execute the rule and get a reward if gas is paid, and the trigger is valid", async () => {
+      // execute valid rule with collateral by someone else. and get a reward.      
+
+    });
+
+    it("Should revert if anyone tries to execute the rule, and the collateral isnt sufficient", async () => {
+      // we get here by calling a valid rule, using up the collateral and call again.
+      
+
+    });
+  });
+
+  describe.skip("Redeem Balance", () => {
+    it("should allow redeeming all the collateral provided if the rule is not yet executed", async () => {
+
+    });
+
+    it("should not allow redeeming collateral if no collateral has been provided by the redeemer", async () => {
+      // provide collateral by someone else.
+    });
+
+    it("should not allow redeeming collateral if the rule was executed and used the collateral", async () => {
+
+    });
+
+    it("should allow redeeming collateral if the rule returned other assets", async () => {
+
+    });
+
+  });
+    
 });
