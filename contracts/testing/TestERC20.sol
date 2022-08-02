d932642018d05806c9fdafd7d6bb29b3fe364ec0
Mad Neutrino <madneutrino@jezer0x.com>
2022-08-02 16:53:22 +0800

    Linted the test contracts

--- contracts/testing/TestERC20.sol
+++ contracts/testing/TestERC20.sol
@@ -5,7 +5,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor(uint256 initialSupply, string memory name, string memory symbol) ERC20(name, symbol) {
+    constructor(
+        uint256 initialSupply,
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
     }
 }
--- contracts/testing/TestOracle.sol
+++ contracts/testing/TestOracle.sol
@@ -8,23 +8,27 @@ pragma solidity ^0.8.9;
 // not actually inheriting it because I dont want to implement the whole thing.abi
 
 contract TestOracle {
-    uint price;
-    constructor(uint _price) {
+    uint256 price;
+
+    constructor(uint256 _price) {
         price = _price;
     }
 
-    function latestRoundData() external
-    view returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    ) {
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
         return (uint80(1), int256(price), uint256(1), uint256(1), uint80(1));
     }
-    
-    function setPrice(uint _price) public {
+
+    function setPrice(uint256 _price) public {
         price = _price;
     }
 }
