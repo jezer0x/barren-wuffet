077c16929ba4001ab0ace58bbdd974f0bbadb80d
psyf <gzsaif001@gmail.com>
2022-08-03 11:46:36 +0800

    removed CollateralTrigger because runTimeParams not supported for Triggers

--- contracts/actions/IAction.sol
+++ contracts/actions/IAction.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "./RETypes.sol";
+
+import "../RETypes.sol";
 
 interface IAction {
     // unpacks action and triggerdata and creates calldata of the callee
--- contracts/actions/SwapUniSingleAction.sol
+++ contracts/actions/SwapUniSingleAction.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "../RETypes.sol";
 import "./IAction.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "../RETypes.sol";
 import "../REConstants.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
