d94aa5900a53cf849ac2552f2400be4a70e52349
psyf <gzsaif001@gmail.com>
2022-08-08 18:11:14 +0800

    renamed fund to trade

--- contracts/TradeManager.sol
+++ contracts/TradeManager.sol
@@ -8,13 +8,13 @@ import "./REConstants.sol";
 import "./RuleExecutor.sol";
 import "./Utils.sol";
 
-contract FundManager is Ownable {
-    event FundCreated(bytes32 indexed fundHash);
-    event Subscribed(bytes32 indexed fundHash, uint256 subIdx);
-    event Unsubscribed(bytes32 indexed fundHash, uint256 subIdx);
-    event RedeemedCancelled(bytes32 indexed fundHash, uint256 subIdx);
-    event RedeemedExecuted(bytes32 indexed fundHash, uint256 subIdx);
-    event Cancelled(bytes32 indexed fundHash);
+contract TradeManager is Ownable {
+    event TradeCreated(bytes32 indexed tradeHash);
+    event Subscribed(bytes32 indexed tradeHash, uint256 subIdx);
+    event Unsubscribed(bytes32 indexed tradeHash, uint256 subIdx);
+    event RedeemedCancelled(bytes32 indexed tradeHash, uint256 subIdx);
+    event RedeemedExecuted(bytes32 indexed tradeHash, uint256 subIdx);
+    event Cancelled(bytes32 indexed tradeHash);
 
     enum SubscriptionStatus {
         ACTIVE,
@@ -22,7 +22,7 @@ contract FundManager is Ownable {
         REDEEMED
     }
 
-    enum FundStatus {
+    enum TradeStatus {
         ACTIVE,
         EXECUTED,
         CANCELLED
@@ -41,28 +41,31 @@ contract FundManager is Ownable {
         uint256 maxCollateralTotal; // limit on subscription to protect from slippage DOS attacks
     }
 
-    struct Fund {
+    struct Trade {
         address manager;
         bytes32 ruleHash;
-        FundStatus status;
+        TradeStatus status;
         SubscriptionConstraints constraints;
         Subscription[] subscriptions;
     }
 
-    mapping(bytes32 => Fund) funds;
+    mapping(bytes32 => Trade) trades;
     RuleExecutor public ruleExecutor;
 
-    modifier onlyActiveSubscriber(bytes32 fundHash, uint256 subscriptionIdx) {
-        require(funds[fundHash].subscriptions[subscriptionIdx].subscriber == msg.sender, "You're not the subscriber!");
+    modifier onlyActiveSubscriber(bytes32 tradeHash, uint256 subscriptionIdx) {
         require(
-            funds[fundHash].subscriptions[subscriptionIdx].status == SubscriptionStatus.ACTIVE,
+            trades[tradeHash].subscriptions[subscriptionIdx].subscriber == msg.sender,
+            "You're not the subscriber!"
+        );
+        require(
+            trades[tradeHash].subscriptions[subscriptionIdx].status == SubscriptionStatus.ACTIVE,
             "This subscription is not active!"
         );
         _;
     }
 
-    modifier onlyFundManager(bytes32 fundHash) {
-        require(funds[fundHash].manager == msg.sender);
+    modifier onlyTradeManager(bytes32 tradeHash) {
+        require(trades[tradeHash].manager == msg.sender);
         _;
     }
 
@@ -75,47 +78,47 @@ contract FundManager is Ownable {
     }
 
     function subscribe(
-        bytes32 fundHash,
+        bytes32 tradeHash,
         address collateralToken,
         uint256 collateralAmount
     ) public payable {
-        Fund storage fund = funds[fundHash];
-        _collectCollateral(fund, collateralToken, collateralAmount);
-        Subscription storage newSub = fund.subscriptions.push();
+        Trade storage trade = trades[tradeHash];
+        _collectCollateral(trade, collateralToken, collateralAmount);
+        Subscription storage newSub = trade.subscriptions.push();
         newSub.subscriber = msg.sender;
         newSub.status = SubscriptionStatus.ACTIVE;
         // TODO: take a fee here
         newSub.collateralAmount = collateralAmount;
-        Rule memory rule = ruleExecutor.getRule(fund.ruleHash);
-        if (rule.totalCollateralAmount >= fund.constraints.minCollateralTotal) {
-            ruleExecutor.activateRule(fund.ruleHash);
+        Rule memory rule = ruleExecutor.getRule(trade.ruleHash);
+        if (rule.totalCollateralAmount >= trade.constraints.minCollateralTotal) {
+            ruleExecutor.activateRule(trade.ruleHash);
         }
-        emit Subscribed(fundHash, fund.subscriptions.length - 1);
+        emit Subscribed(tradeHash, trade.subscriptions.length - 1);
     }
 
     function _collectCollateral(
-        Fund memory fund,
+        Trade memory trade,
         address collateralToken,
         uint256 collateralAmount
     ) private {
-        _validateCollateral(fund, collateralToken, collateralAmount);
+        _validateCollateral(trade, collateralToken, collateralAmount);
         if (collateralToken != REConstants.ETH) {
             IERC20(collateralToken).transferFrom(msg.sender, address(this), collateralAmount);
             IERC20(collateralToken).approve(address(ruleExecutor), collateralAmount);
-            ruleExecutor.addCollateral(fund.ruleHash, collateralAmount);
+            ruleExecutor.addCollateral(trade.ruleHash, collateralAmount);
         } else {
             // else it should be in our balance already
-            ruleExecutor.addCollateral{value: msg.value}(fund.ruleHash, collateralAmount);
+            ruleExecutor.addCollateral{value: msg.value}(trade.ruleHash, collateralAmount);
         }
     }
 
     function _validateCollateral(
-        Fund memory fund,
+        Trade memory trade,
         address collateralToken,
         uint256 collateralAmount
     ) private view {
-        Rule memory rule = ruleExecutor.getRule(fund.ruleHash);
-        SubscriptionConstraints memory constraints = fund.constraints;
+        Rule memory rule = ruleExecutor.getRule(trade.ruleHash);
+        SubscriptionConstraints memory constraints = trade.constraints;
         require(rule.actions[0].fromToken == collateralToken, "Wrong Collateral Type");
         require(constraints.minCollateralPerSub <= collateralAmount, "Insufficient Collateral for Subscription");
         require(constraints.maxCollateralPerSub >= collateralAmount, "Max Collateral for Subscription exceeded");
@@ -129,66 +132,66 @@ contract FundManager is Ownable {
         }
     }
 
-    function unsubscribe(bytes32 fundHash, uint256 subscriptionIdx)
+    function unsubscribe(bytes32 tradeHash, uint256 subscriptionIdx)
         public
-        onlyActiveSubscriber(fundHash, subscriptionIdx)
+        onlyActiveSubscriber(tradeHash, subscriptionIdx)
     {
-        Fund storage fund = funds[fundHash];
-        bytes32 ruleHash = fund.ruleHash;
+        Trade storage trade = trades[tradeHash];
+        bytes32 ruleHash = trade.ruleHash;
         Rule memory rule = ruleExecutor.getRule(ruleHash);
-        Subscription storage subscription = fund.subscriptions[subscriptionIdx];
+        Subscription storage subscription = trade.subscriptions[subscriptionIdx];
         require(rule.status == RuleStatus.ACTIVE || rule.status == RuleStatus.PAUSED, "unsubscribe failed");
         ruleExecutor.reduceCollateral(ruleHash, subscription.collateralAmount);
         Utils._send(subscription.subscriber, subscription.collateralAmount, rule.actions[0].fromToken);
         subscription.status = SubscriptionStatus.CANCELLED;
 
-        if (rule.status == RuleStatus.ACTIVE && rule.totalCollateralAmount < fund.constraints.minCollateralTotal) {
+        if (rule.status == RuleStatus.ACTIVE && rule.totalCollateralAmount < trade.constraints.minCollateralTotal) {
             ruleExecutor.pauseRule(ruleHash);
         }
 
-        emit Unsubscribed(fundHash, subscriptionIdx);
+        emit Unsubscribed(tradeHash, subscriptionIdx);
     }
 
-    function cancelFund(bytes32 fundHash) public onlyFundManager(fundHash) {
-        Fund storage fund = funds[fundHash];
-        fund.status = FundStatus.CANCELLED;
-        ruleExecutor.cancelRule(fund.ruleHash);
-        emit Cancelled(fundHash);
+    function cancelTrade(bytes32 tradeHash) public onlyTradeManager(tradeHash) {
+        Trade storage trade = trades[tradeHash];
+        trade.status = TradeStatus.CANCELLED;
+        ruleExecutor.cancelRule(trade.ruleHash);
+        emit Cancelled(tradeHash);
     }
 
-    function redeemCollateralFromCancelledFund(bytes32 fundHash, uint256 subscriptionIdx)
+    function redeemCollateralFromCancelledTrade(bytes32 tradeHash, uint256 subscriptionIdx)
         public
-        onlyActiveSubscriber(fundHash, subscriptionIdx)
+        onlyActiveSubscriber(tradeHash, subscriptionIdx)
     {
-        Fund storage fund = funds[fundHash];
-        bytes32 ruleHash = fund.ruleHash;
+        Trade storage trade = trades[tradeHash];
+        bytes32 ruleHash = trade.ruleHash;
         Rule memory rule = ruleExecutor.getRule(ruleHash);
-        Subscription storage subscription = fund.subscriptions[subscriptionIdx];
-        require(fund.status == FundStatus.CANCELLED, "Fund is not cancelled!");
-        // This contract should have the collateral back already since it was cancelled by fund.manager
+        Subscription storage subscription = trade.subscriptions[subscriptionIdx];
+        require(trade.status == TradeStatus.CANCELLED, "Trade is not cancelled!");
+        // This contract should have the collateral back already since it was cancelled by trade.manager
         Utils._send(subscription.subscriber, subscription.collateralAmount, rule.actions[0].fromToken);
         subscription.status = SubscriptionStatus.REDEEMED;
-        emit RedeemedCancelled(fundHash, subscriptionIdx);
+        emit RedeemedCancelled(tradeHash, subscriptionIdx);
     }
 
-    function redeemOutputFromExecutedFund(bytes32 fundHash, uint256 subscriptionIdx)
+    function redeemOutputFromExecutedTrade(bytes32 tradeHash, uint256 subscriptionIdx)
         public
-        onlyActiveSubscriber(fundHash, subscriptionIdx)
+        onlyActiveSubscriber(tradeHash, subscriptionIdx)
     {
-        Fund storage fund = funds[fundHash];
-        bytes32 ruleHash = fund.ruleHash;
+        Trade storage trade = trades[tradeHash];
+        bytes32 ruleHash = trade.ruleHash;
         Rule memory rule = ruleExecutor.getRule(ruleHash);
-        Subscription storage subscription = fund.subscriptions[subscriptionIdx];
+        Subscription storage subscription = trade.subscriptions[subscriptionIdx];
 
-        if (rule.status == RuleStatus.EXECUTED && fund.status != FundStatus.EXECUTED) {
-            // When rule was executed but fund doesn't know
+        if (rule.status == RuleStatus.EXECUTED && trade.status != TradeStatus.EXECUTED) {
+            // When rule was executed but trade doesn't know
             // first time subscriber wants to get back the output, we change this
-            // TODO: maybe doing it in the ruleExecution->fund callback would be a better idea?
+            // TODO: maybe doing it in the ruleExecution->trade callback would be a better idea?
             ruleExecutor.redeemBalance(ruleHash);
-            fund.status = FundStatus.EXECUTED;
+            trade.status = TradeStatus.EXECUTED;
         }
 
-        require(fund.status == FundStatus.EXECUTED, "Rule hasn't been executed yet!");
+        require(trade.status == TradeStatus.EXECUTED, "Rule hasn't been executed yet!");
 
         uint256 balance = (subscription.collateralAmount * rule.outputAmount) / rule.totalCollateralAmount; // TODO: make sure the math is fine, especially at the boundaries
         Utils._send(subscription.subscriber, balance, rule.actions[rule.actions.length - 1].toToken);
@@ -196,24 +199,24 @@ contract FundManager is Ownable {
         emit RedeemedExecuted(ruleHash, subscriptionIdx);
     }
 
-    function hashFund(address manager, bytes32 ruleHash) public pure returns (bytes32) {
+    function hashTrade(address manager, bytes32 ruleHash) public pure returns (bytes32) {
         return keccak256(abi.encode(manager, ruleHash));
     }
 
-    function createFund(
+    function createTrade(
         Trigger[] calldata triggers,
         Action[] calldata actions,
         SubscriptionConstraints calldata constraints
     ) public payable returns (bytes32) {
         bytes32 ruleHash = ruleExecutor.createRule{value: msg.value}(triggers, actions);
-        bytes32 fundHash = hashFund(msg.sender, ruleHash);
-        Fund storage fund = funds[fundHash];
-        fund.manager = msg.sender;
-        fund.ruleHash = ruleHash;
-        fund.status = FundStatus.ACTIVE;
-        fund.constraints = constraints;
+        bytes32 tradeHash = hashTrade(msg.sender, ruleHash);
+        Trade storage trade = trades[tradeHash];
+        trade.manager = msg.sender;
+        trade.ruleHash = ruleHash;
+        trade.status = TradeStatus.ACTIVE;
+        trade.constraints = constraints;
 
-        emit FundCreated(fundHash);
-        return fundHash;
+        emit TradeCreated(tradeHash);
+        return tradeHash;
     }
 }
