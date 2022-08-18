7f44f8ca3449cf2a667022cee8f326eed7551350
Mad Neutrino <madneutrino@jezer0x.com>
2022-08-18 13:19:18 +0800

    Fixed UNI_PRICE_IN_ETH. It was actually referring to ETH_PRICE_IN_UNI earlier.

--- test/Constants.ts
+++ test/Constants.ts
@@ -8,8 +8,9 @@ export const PRICE_TRIGGER_DECIMALS = BigNumber.from(10).pow(8);
 
 export const ETH_PRICE_IN_USD = BigNumber.from(1300).mul(PRICE_TRIGGER_DECIMALS);
 export const UNI_PRICE_IN_USD = BigNumber.from(3).mul(PRICE_TRIGGER_DECIMALS);
-export const UNI_PRICE_IN_ETH_PARAM = utils.defaultAbiCoder.encode(["string", "string"], ["eth", "uni"]);
-export const UNI_PRICE_IN_ETH = ETH_PRICE_IN_USD.mul(PRICE_TRIGGER_DECIMALS).div(UNI_PRICE_IN_USD); // 43333333333 = ~433 UNI can be bought per ETH
+
+export const UNI_PRICE_IN_ETH_PARAM = utils.defaultAbiCoder.encode(["string", "string"], ["uni", "eth"]);
+export const UNI_PRICE_IN_ETH = UNI_PRICE_IN_USD.mul(PRICE_TRIGGER_DECIMALS).div(ETH_PRICE_IN_USD); // 230769 = 1 UNI costs ~0.00230769 ETH
 
 export const BAD_RULE_HASH = "0x" + "1234".repeat(16);
 
