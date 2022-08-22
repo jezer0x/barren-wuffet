3b753cec4093b9ec127b6ea09d67178e3b15f208
psyf <gzsaif001@gmail.com>
2022-08-22 15:57:06 +0800

    add explanations for the TradeStatuses

--- contracts/trades/TradeTypes.sol
+++ contracts/trades/TradeTypes.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.9;
 import "../utils/subscriptions/SubscriptionTypes.sol";
 
 enum TradeStatus {
-    ACTIVE,
-    REDEEMABLE,
-    EXECUTED,
-    CANCELLED
+    ACTIVE, // Deposits possible, withdraws possible, Rule has not been executed
+    REDEEMABLE, // Deposits not possible, withdraws not possible, Rule has been executed but someone needs to call redeemRuleOuput
+    EXECUTED, // Deposits not possible, withdraws possible (outputToken), Rule has been executed and ruleOutput redeemed.
+    CANCELLED // Deposits not possible, withdraws possible (inputToken), Rule has been cancelled
 }
 
 struct Trade {
