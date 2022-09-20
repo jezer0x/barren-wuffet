511207d3f445f13708314a12f7c216360d0da1d3
psyf <gzsaif001@gmail.com>
2022-09-20 09:45:26 +0800

    robocop fillup, copypasta for now

--- subgraph/src/robo-cop.ts
+++ subgraph/src/robo-cop.ts
@@ -9,9 +9,10 @@ import {
   OwnershipTransferred as OwnershipTransferredEvent,
   PositionCreated as PositionCreatedEvent,
   PositionsClosed as PositionsClosedEvent,
-  Redeemed as RedeemedEvent,
-} from "../generated/RoboCop/RoboCop";
-import { Fund } from "../generated/schema";
+  Redeemed as RedeemedEvent
+} from "../generated/templates/RoboCop/RoboCop";
+import { Fund, Position } from "../generated/schema";
+import { Bytes } from "@graphprotocol/graph-ts";
 
 export function handleActivated(event: ActivatedEvent): void {}
 
@@ -19,7 +20,16 @@ export function handleCollateralAdded(event: CollateralAddedEvent): void {}
 
 export function handleCollateralReduced(event: CollateralReducedEvent): void {}
 
-export function handleCreated(event: CreatedEvent): void {}
+export function handleCreated(event: CreatedEvent): void {
+  let entity = Fund.load(event.address);
+
+  if (!entity) {
+    throw Error;
+  }
+
+  entity.rules.push(event.params.ruleHash);
+  entity.save();
+}
 
 export function handleDeactivated(event: DeactivatedEvent): void {}
 
@@ -29,8 +39,45 @@ export function handleInitialized(event: InitializedEvent): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {}
 
-export function handlePositionCreated(event: PositionCreatedEvent): void {}
+export function handlePositionCreated(event: PositionCreatedEvent): void {
+  let fund = Fund.load(event.address);
 
-export function handlePositionsClosed(event: PositionsClosedEvent): void {}
+  if (!fund) {
+    throw Error;
+  }
+
+  let position = new Position(event.params.positionHash);
+  position.next_actions = event.params.nextActions;
+
+  fund.fund_pending_positions.push(position.id);
+
+  position.save();
+  fund.save();
+}
+
+export function handlePositionsClosed(event: PositionsClosedEvent): void {
+  let fund = Fund.load(event.address);
+
+  if (!fund) {
+    throw Error;
+  }
+
+  event.params.positionHashesClosed.forEach(function(positionHash) {
+    removeFromPendingPositions(fund.fund_pending_positions, positionHash);
+  });
+
+  fund.save();
+}
 
 export function handleRedeemed(event: RedeemedEvent): void {}
+
+function removeFromPendingPositions(arr: Bytes[], hash: Bytes) {
+  var i: number;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == hash) {
+      arr[i] = arr[arr.length - 1];
+      arr.pop();
+      break;
+    }
+  }
+}
