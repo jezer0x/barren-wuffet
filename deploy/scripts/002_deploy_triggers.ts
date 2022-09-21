e753cadfe30cca7f0fc0e5c33339311515b76477
psyf <gzsaif001@gmail.com>
2022-09-21 11:06:27 +0800

    make trigger/action deployments more modular

--- deploy/scripts/002_deploy_triggers.ts
+++ deploy/scripts/002_deploy_triggers.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
+import { Contract } from "ethers";
 
 const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
@@ -15,16 +16,21 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
     // loose test for "that that whitelist was already created"
   }
 
+  await deployPriceTrigger(deploy, deployer, whitelistService, trigWlHash);
+  // TODO: Deploy Timestamp Trigger
+};
+
+async function deployPriceTrigger(deploy: any, deployer: string, whitelistService: Contract, trigWlHash: any) {
   const priceTrigger = await deploy("PriceTrigger", {
     from: deployer,
     args: [],
     log: true
   });
 
-  await whitelistService.addToWhitelist(trigWlHash, priceTrigger.address);
-
-  // TODO: Deploy Timestamp Trigger
-};
+  if (!(await whitelistService.isWhitelisted(trigWlHash, priceTrigger.address))) {
+    await whitelistService.addToWhitelist(trigWlHash, priceTrigger.address);
+  }
+}
 
 export default func;
 func.tags = ["Triggers"];
--- deploy/scripts/003_deploy_actions.ts
+++ deploy/scripts/003_deploy_actions.ts
@@ -2,16 +2,13 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
 import { getLibraries } from "../utils";
+import { Contract } from "ethers";
 
 const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, getChainId } = hre;
+  const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   const { TokenLibAddr } = await getLibraries();
-
-  // This is being done for tests
-  const uniswapAddr = (await ethers.getContract("TestSwapRouter")).address;
-  const weth9Addr = (await ethers.getContract("WETH")).address;
 
   const whitelistService = await ethers.getContract("WhitelistService");
   const actWlHash = await whitelistService.getWhitelistHash(deployer, "actions");
@@ -21,6 +18,21 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
     // loose test for "that that whitelist was already created"
   }
 
+  await deploySwapUniSingleAction(deploy, deployer, whitelistService, actWlHash, TokenLibAddr);
+  // TODO: deploy all the other actions
+};
+
+async function deploySwapUniSingleAction(
+  deploy: any,
+  deployer: string,
+  whitelistService: Contract,
+  actWlHash: any,
+  TokenLibAddr: string
+) {
+  // TODO: utils to change the following to vars, depending on chainID
+  const uniswapAddr = (await ethers.getContract("TestSwapRouter")).address;
+  const weth9Addr = (await ethers.getContract("WETH")).address;
+
   const swapUniSingleAction = await deploy("SwapUniSingleAction", {
     from: deployer,
     args: [uniswapAddr, weth9Addr],
@@ -28,10 +40,10 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
     libraries: { TokenLib: TokenLibAddr }
   });
 
-  await whitelistService.addToWhitelist(actWlHash, swapUniSingleAction.address);
-
-  // TODO: deploy all the other actions
-};
+  if (!(await whitelistService.isWhitelisted(actWlHash, swapUniSingleAction.address))) {
+    await whitelistService.addToWhitelist(actWlHash, swapUniSingleAction.address);
+  }
+}
 
 export default func;
 func.tags = ["Actions"];
