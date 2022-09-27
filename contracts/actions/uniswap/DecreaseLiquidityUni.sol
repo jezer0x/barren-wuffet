9fdc0c99b05a35ad6225400c7d4f87e085684119
psyf <gzsaif001@gmail.com>
2022-09-27 15:23:54 +0800

    decrease liquidity

--- contracts/actions/uniswap/DecreaseLiquidityUni.sol
+++ contracts/actions/uniswap/DecreaseLiquidityUni.sol
@@ -12,12 +12,12 @@ import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.s
 
 /*
     Reference: 
-        https://docs.uniswap.org/protocol/guides/providing-liquidity/the-full-contract
+        https://docs.uniswap.org/protocol/guides/providing-liquidity/decrease-liquidity
 
     Tokens: 
-        Will only have 1 input tokens (NFT) and 2 outputs (fees in token0 and token1)
+        Will only have 1 input tokens (NFT) and 2 outputs (amount taken out in token0 and token1)
 */
-contract CollectFeesUni is IAction, DelegatePerform {
+contract DecreaseLiquidityUni is IAction, DelegatePerform {
     using SafeERC20 for IERC20;
 
     INonfungiblePositionManager immutable nonfungiblePositionManager;
@@ -35,12 +35,30 @@ contract CollectFeesUni is IAction, DelegatePerform {
         require(action.outputTokens[0].t == TokenType.ERC20 || action.outputTokens[0].t == TokenType.NATIVE);
         require(action.outputTokens[1].t == TokenType.ERC20 || action.outputTokens[1].t == TokenType.NATIVE);
 
-        (, , address token0, address token1, , , , , , , , ) = nonfungiblePositionManager.positions(
-            action.inputTokens[0].id
-        );
+        (
+            ,
+            ,
+            address token0,
+            address token1,
+            ,
+            ,
+            ,
+            uint128 liquidity,
+            ,
+            ,
+            uint256 tokens0Owed,
+            uint256 tokens1Owed
+        ) = nonfungiblePositionManager.positions(action.inputTokens[0].id);
 
         require(token0 == action.outputTokens[0].addr);
         require(token1 == action.outputTokens[1].addr);
+        (uint128 _liquidity, uint256 _amount0Min, uint256 _amount1Min) = abi.decode(
+            action.data,
+            (uint128, uint256, uint256)
+        );
+        require(liquidityToWithdraw >= liquidity);
+        require(tokens0Owed >= _amount0Min);
+        require(tokens1Owed >= _amount1Min);
 
         return true;
     }
@@ -52,16 +70,20 @@ contract CollectFeesUni is IAction, DelegatePerform {
     {
         uint256[] memory outputs = new uint256[](2);
 
-        (uint128 _amount0Max, uint128 _amount1Max) = abi.decode(action.data, (uint128, uint128));
+        (uint128 _liquidity, uint256 _amount0Min, uint256 _amount1Min) = abi.decode(
+            action.data,
+            (uint128, uint256, uint256)
+        );
+        INonfungiblePositionManager.DecreaseLiquidityParams memory params = INonfungiblePositionManager
+            .DecreaseLiquidityParams({
+                tokenId: action.inputTokens[0].id,
+                liquidity: _liquidity,
+                amount0Min: _amount0Min,
+                amount1Min: _amount1Min,
+                deadline: block.timestmap
+            });
 
-        INonfungiblePositionManager.CollectParams memory params = INonfungiblePositionManager.CollectParams({
-            tokenId: action.inputTokens[0].id,
-            recipient: address(this),
-            amount0Max: _amount0Max,
-            amount1Max: _amount1Max
-        });
-
-        (uint256 amount0, uint256 amount1) = nonfungiblePositionManager.collect(params);
+        (uint256 amount0, uint256 amount1) = nonfungiblePositionManager.decreaseLiquidity(params);
 
         outputs[0] = amount0;
         outputs[1] = amount1;
