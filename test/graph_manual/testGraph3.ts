1ca5d7f093042539be3abb4b3d793ec7d700d412
Mad Neutrino <madneutrino@jezer0x.com>
2022-10-13 13:03:39 +0800

    Clarify the instructions further. fix import issue in testGraph3.ts

--- test/graph_manual/testGraph3.ts
+++ test/graph_manual/testGraph3.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
-import { ETH_TOKEN, ERC20_DECIMALS } from "../test/Constants";
+import { ETH_TOKEN, ERC20_DECIMALS } from "../Constants";
 
 async function main() {
   const McFund = await ethers.getContractAt("Fund", "0xec4cfde48eadca2bc63e94bb437bbeace1371bf3");
